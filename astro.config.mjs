35e59b17433b8f795c2f8603d1c2398dd0f73887
iyokan7 <iyokan2022.08.13@gmail.com>
2024-09-12 22:08:18 +0900

    PostとGetを使用してページ間の値の受け渡しを出来るようにする練習

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://iyokan.com",
-  integrations: [preact()]
+  integrations: [preact()],
+  output: "server",
+  adapter: netlify()
 });
